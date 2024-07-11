54663c5647e964d8bfa2a4a9698b265dab0a0bd4
eclipse021 <junhyeon@naver.com>
2024-07-11 13:35:09 +0900

    원래 App.js로 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+/*import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
@@ -15,6 +15,32 @@ function App() {
         백엔드 데이터 : {hello}
       </div>
   );
+}
+
+export default App;*/
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
